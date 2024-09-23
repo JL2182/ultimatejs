927ac6b15525d71a88086f55098033f6bfad151d
_Jose_Luis_ <geo.ucedo@gmail.com>
2024-09-23 15:55:06 -0400

    Avance hasta el archivo 08-funciones.

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,10 @@
+let = numero = 1; //tipo número
+let texto = 'Hola Mundo';//tipo string
+let verdadero = true; //tipo boolean
+let falso = false; //tipo boolean
+let noDefinido; //undefined
+let undef = undefined; // puede ser dos cosas: tipo de dato o valor.
+let nulo = null; //normalmente se usa en combo de opciones en donde el usuario no eligio ninguna opción.
+
+
+
--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hola Mundo')
+let nombre = 'Hola Mundo'; // creación de variables; si es que no se especifica el valor de la variable, en la consola se mostrara la palabra  undefined.
+
+console.log(nombre);
+
+//convenciones para definir variables 
+//1 UpperCamelCase =  primera letra en mayusculas y el resto en minusculas
+
+//2 cameñCase =  primera letra en minuscula y el resto en minusculas. la  convención mas utilizada.
+
+
+//3 snake_case =   todas las letras en minusculas y separadas por guiones bajos
+
+let NombreCompleto;
+let nombreCompleto;
+let nombre_completo;
+
+let apellido; //esta liena crea la  variable apellido y le asigna el valor undefined. 
+
+apellido = 'Schurmann'; //esta linea  asigna el valor 'Schurmann' a la variable apellido.
+
+
+
